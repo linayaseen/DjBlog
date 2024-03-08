95c2c852b9c1a7763c4d5fd3725714ed66b6cd25
linayaseen <linayaseen@outlook.com>
2024-03-08 15:57:54 +0200

    add admin customization

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+'''
+posts:
+- title
+- content
+- draft
+- publish date
+- uther
+- image
+- tags
+- category
+- comments
+
+'''
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField(max_length=20000)
+    draft=models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.title
